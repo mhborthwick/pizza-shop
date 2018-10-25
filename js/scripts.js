65dace0fca3039f353b58a2bb23c1b256d6deb0a
Michael Borthwick <mhborthwick@gmail.com>
2018-10-25 13:33:27 -0700

    get order amount to update correctly without refresh

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // biz logic
-var customerName;
+// var customerName;
 var toppingOptions;
 var toppingOptions2;
 var pizzaSizeOption;
@@ -7,8 +7,8 @@ var arr = [];
 var arr2 = [];
 // get array of topping items + convert into numerical value;
 
-function Pizza (){
-  this.name = "";
+function Pizza (name){
+  this.name = name;
   this.toppingTotal = 0;
   this.size = 0;
   this.currentTotal = 0;
@@ -17,7 +17,7 @@ function Pizza (){
   // this.OverallTotal = 0;
 }
 
-var customer = new Pizza();
+var customer = new Pizza("Mike");
 
 Pizza.prototype.countarrLength = function(){
   return this.toppingTotal = arr.length * 1 + arr2.length * 3;
@@ -31,15 +31,22 @@ Pizza.prototype.addCurrentAmount = function() {
   return this.currentTotal = this.toppingTotal + this.size;
 }
 
-Pizza.prototype.addName = function (){
-  return this.name = customerName;
-}
+// Pizza.prototype.addName = function (){
+//   return this.name = customerName;
+// }
 
+function resetFields(){
+    arr.length = 0;
+    arr2.length = 0;
+}
 
 // user logic
 $(document).ready(function(){
+
   $("#selectOrder").submit(function(event) {
     event.preventDefault();
+
+    resetFields();
 
     $("input:checkbox[name=toppingOption]:checked").each(function(){
       toppingOptions = $(this).val();
@@ -51,19 +58,25 @@ $(document).ready(function(){
       arr2.push(toppingOptions2);
     });
 
+
     pizzaSizeOption = $("#sizePizza").val();
-    customerName = $("#custName").val()
+    // customerName = $("#custName").val()
 
     customer.countarrLength();
     customer.sizeSelect();
     customer.addCurrentAmount();
-    customer.addName();
+    // customer.addName();
 
     console.log(customer);
     // console.log(customerName);
     // console.log(pizzaSizeOption);
 
+    $("#customerName").text(" " + customer.name);
     $("#orderAmount").text(" " + "$" + customer.currentTotal);
+
+    $("#result").css("display", "block");
+
+
 
   });
 });
