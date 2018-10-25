0f76420ab40097f4c5bf8a54a1b74b1880720ef2
Michael Borthwick <mhborthwick@gmail.com>
2018-10-24 23:09:33 -0700

    get form button to display console log on submit

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+// biz logic
+// var toppingTotalAmount;
+// get array of topping items + convert into numerical value;
+
+// function Pizza (){
+//   this.toppingTotal = toppingTotalAmount;
+//   this.size = 0;
+//   this.currentTotal = 0;
+//   this.OverallTotal = 0;
+// }
+//
+// var customer = new Pizza();
+//
+// Player.prototype.addCurrentAmount = function() {
+//   return this.currentTotal = toppingTotalAmount + this.size;
+// }
+
+// user logic
+$(document).ready(function(){
+  $("#selectOrder").submit(function(event) {
+    event.preventDefault();
+
+    console.log("hey");
+
+
+  });
+});
