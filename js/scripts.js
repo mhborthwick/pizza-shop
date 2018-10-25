05e8ac9f4681bd2814223a5785ca086b40c0db2e
Michael Borthwick <mhborthwick@gmail.com>
2018-10-25 00:08:59 -0700

    create checkbox form with toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 // biz logic
-// var toppingTotalAmount;
+var toppingTotalAmount;
 // get array of topping items + convert into numerical value;
 
-// function Pizza (){
-//   this.toppingTotal = toppingTotalAmount;
-//   this.size = 0;
-//   this.currentTotal = 0;
-//   this.OverallTotal = 0;
-// }
-//
-// var customer = new Pizza();
-//
-// Player.prototype.addCurrentAmount = function() {
-//   return this.currentTotal = toppingTotalAmount + this.size;
-// }
+function Pizza (){
+  this.toppingTotal = toppingTotalAmount;
+  this.size = 0;
+  this.currentTotal = 0;
+  this.OverallTotal = 0;
+}
+
+var customer = new Pizza();
+
+Pizza.prototype.addCurrentAmount = function() {
+  return this.currentTotal = toppingTotalAmount + this.size;
+}
 
 // user logic
 $(document).ready(function(){
